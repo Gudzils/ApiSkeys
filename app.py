bfb48185b2f1efa54cecb00c120da72a578f662e
Gudzils <krmpgod@gmail.com>
2025-05-29 22:37:19 +0500

    app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+import random
+
+app = Flask(__name__)
+
+# Хранилище ключей
+api_keys = set()
+
+# Главная страница API
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    return jsonify({"message": "Hello Friend"})
+
+# Генерация нового ключа
+@app.route('/api/generate', methods=['GET'])
+def generate_key():
+    key = "KeyGls" + ''.join(str(random.randint(0, 9)) for _ in range(8))
+    api_keys.add(key)
+    return jsonify({"api_key": key})
+
+# Проверка ключа (передается в URL)
+@app.route('/api/check/<string:key>', methods=['GET'])
+def check_key(key):
+    if key in api_keys and key.startswith("Atlant_hub"):
+        return jsonify({"status": "valid"})
+    else:
+        return jsonify({"status": "invalid"}), 401
+
+if __name__ == '__main__':
+    app.run(debug=True)
